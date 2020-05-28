a05f90ae10ee349cb92cd091dd46644e81342845
Flora Petersen <floramarina@comcast.net>
2020-05-28 20:16:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,9 +8,9 @@ end
 
 def countdown_with_sleep
   while number > 0
-    puts "#{number} SECONDS!"
-    sleep(1)
-    number -= 1
+    sleep 1
+      puts "#{number} SECONDS!"
+      number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
