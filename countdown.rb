ebc435c5e88018ccbf76910f8b71d3884587cd31
Flora Petersen <floramarina@comcast.net>
2020-05-28 20:14:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
 def countdown(number)
-while number > 0
-  puts "#{number} SECONDS!"
-  number -= 1
-end
-  "HAPPY NEW YEAR!"
+  while number > 0
+    puts "#{number} SECONDS!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
-while number > 0
-  puts "#{number} SECONDS!"
-  sleep(1)
-  number -= 1
-end
-  "HAPPY NEW YEAR!"
+  while number > 0
+    puts "#{number} SECONDS!"
+    sleep(1)
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
